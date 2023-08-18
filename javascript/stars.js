da62cafc07e84d41989e898c757f2056114a7193
saleemchisti <139341298+saleemchisti@users.noreply.github.com>
2023-08-18 17:07:04 +0530

    Add files via upload

---
+++ javascript/stars.js
@@ -0,0 +1,24 @@
+var images=document.getElementsByClassName("a")
+for(i=1;i<=images.length;i++)
+{
+    images[i-1].setAttribute("src","../asset/images/stars/"+i+".jpg")
+    images[i-1].style.width="220px"
+    images[i-1].style.height="220px"
+}
+console.log(images)
+// this is for header
+var indval=0;
+var imagesarr=["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg"]
+ 
+ function openImg(pic,name,info)
+ {
+    document.getElementById("subWindow").removeAttribute("style")
+    document.getElementById("subWindowImg").src='../asset/images/stars/'+pic+".jpg"
+    document.getElementById("imageHeading").innerHTML=name;
+    document.getElementById("imgpara").innerHTML=info;
+ }
+ function closeImg()
+ {
+    document.getElementById("subWindow").setAttribute("style","display:none")
+
+ }
